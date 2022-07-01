f0528d6788c068162fe58c4009bcd5208099787c
Max <maxotuteye12@gmail.com>
2022-07-01 20:28:27 +0000

    added env

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const urlShortener = require('node-url-shortener')
-const port = 3000
+const port = process.env.PORT || 3000
 const path = require('path')
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
