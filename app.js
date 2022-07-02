cac7825573ccdb213b0bcb9e5e32847b8b9562bb
Max <maxotuteye12@gmail.com>
2022-07-02 09:49:42 +0000

    handlebars

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const urlShortener = require('node-url-shortener')
+const db = require('./models/index')
+const exphbs = require('express-handlebars')
+
 const port = process.env.PORT || 3000
 const path = require('path')
 const app = express()
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.urlencoded())
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'))
@@ -13,8 +20,12 @@ app.get('/', function (req, res) {
 
 app.post('/url', function (req, res) {
     const url = req.body.url
+
     urlShortener.short(url, function (err, shortUrl) {
-        res.send(shortUrl)
+        db.Url.findOrCreate({where: {url: url, shortUrl: shortUrl}})
+            .then(([urlObj, created]) => {
+                res.send(shortUrl)
+            })
     })
 })
 
